535bbb22f001206de745fe152068eb0536f21897
Daniil Kiselev <danqkiselev@yandex.ru>
2023-07-01 22:07:59 +0300

    Installed Hermone and made 1 test

--- .hermione.conf.js
+++ .hermione.conf.js
@@ -1,22 +1,35 @@
 module.exports = {
+  gridUrl: 'http://localhost:4444/wd/hub',
+  baseUrl: 'http://localhost',
+  pageLoadTimeout: 0,
+  httpTimeout: 60000,
+  testTimeout: 90000,
+  resetCursor: false,
   sets: {
-    desktop: {
-      files: "test/hermione",
-    },
+      desktop: {
+          files: [
+              'test/**/*.hermione.js'
+          ],
+          browsers: [
+              'chrome'
+          ]
+      }
   },
-
   browsers: {
-    chrome: {
-      automationProtocol: "devtools",
-      desiredCapabilities: {
-        browserName: "chrome",
-      },
-    },
+      chrome: {
+          automationProtocol: 'devtools',
+          desiredCapabilities: {
+              browserName: 'chrome'
+          }
+      }
   },
   plugins: {
-    "html-reporter/hermione": {
-      enabled: true,
-      // path:'hermione-html-report'
-    },
-  },
-};
+      'html-reporter/hermione': {
+          // https://github.com/gemini-testing/html-reporter
+          enabled: true,
+          path: 'hermione-report',
+          defaultView: 'all',
+          diffMode: '3-up-scaled'
+      }
+  }
+}
--- test/hermione/example.hermione.js
+++ test/hermione/example.hermione.js
@@ -1,14 +1,14 @@
 const { assert } = require('chai');
 
-describe('microsoft', async function() {
-    it('Тест, который пройдет', async function() {
-        await this.browser.url('https://www.microsoft.com/ru-ru/');
-        await this.browser.assertView('plain', 'body');
+// describe('microsoft', async function() {
+//     it('Тест, который пройдет', async function() {
+//         await this.browser.url('https://www.microsoft.com/ru-ru/');
+//         await this.browser.assertView('plain', 'body');
 
-        const title = await this.browser.$('#uhfLogo').getText();
-        assert.equal(title, 'Microsoft');
-    });
-});
+//         const title = await this.browser.$('#uhfLogo').getText();
+//         assert.equal(title, 'Microsoft');
+//     });
+// });
 
 
 // describe('Конвертор валют', ()=>{
@@ -16,7 +16,7 @@ describe('microsoft', async function() {
 //         const puppeteer = await browser.getPuppeteer(); //отключение через фрэйм и удобно по этому протоколу взаимод с браузером
 //         const [page] = await puppeteer.pages() //выбираем первую вкладку
 
-//         await page.goto('https://ya.ru') //совершаем переход на сайт
+//         await page.goto('http://localhost:3000/hw/store/') //совершаем переход на сайт
 //         await page.keyboard.type('Курс доллара к рублю') //в элементе который в фокусе вводим
 //         await page.keyboard.press('Enter')
 //         // await browser.pause(1000) //ожидаем прогрузки
@@ -28,3 +28,33 @@ describe('microsoft', async function() {
 //         await browser.asserView('plain', '.Convertor') //, {ignoreElements: [css Selectors]}
 //     })
 // })
+describe('Конвертор валют', ()=>{
+    it('в магазине должны быть страницы: главная, каталог, условия доставки, контакты', async({browser})=>{
+        // const puppeteer = await browser.getPuppeteer(); //отключение через фрэйм и удобно по этому протоколу взаимод с браузером
+        // const [page] = await puppeteer.pages() //выбираем первую вкладку
+        await browser.url('http://localhost:3000/hw/store/')
+        const mainText = await browser.$('[data-testid="page-name"]').getText()
+        assert.equal(mainText, 'Welcome to Example store!');
+
+        await browser.url('http://localhost:3000/hw/store/catalog')
+        const catalogText = await browser.$('[data-testid="page-name"]').getText()
+        assert.equal(catalogText, 'Catalog');
+
+        await browser.url('http://localhost:3000/hw/store/delivery')
+        const deliveryText = await browser.$('[data-testid="page-name"]').getText()
+        assert.equal(deliveryText, 'Delivery');
+
+        await browser.url('http://localhost:3000/hw/store/contacts')
+        const contactsText = await browser.$('[data-testid="page-name"]').getText()
+        assert.equal(contactsText, 'Contacts');
+        
+
+
+
+
+
+
+
+      
+    })
+})
